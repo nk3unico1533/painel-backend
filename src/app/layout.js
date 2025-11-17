1bfe432f063bcb85e59202d0bfead80de242ad55
nk3unico1533 <nk3unico@outlook.com>
2025-11-16 22:12:54 -0300

    Atualizar o layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,19 +3,18 @@ import Header from "../components/Header";
 
 export const metadata = {
   title: "Painel Consulta",
-  description: "Painel de consultas — Dark Neon"
+  description: "Painel de consultas — Dark Neon",
 };
 
 export default function RootLayout({ children }) {
-  // read basic session from localStorage in client side components (Header is client)
   return (
     <html lang="pt-BR">
       <body>
-        <div className="container">
-          <Header user={null} />
-          {children}
-          <div className="footer-small">© {new Date().getFullYear()} Painel Consulta</div>
-        </div>
+        <Header />
+        <main>{children}</main>
+        <footer className="footer-small">
+          © {new Date().getFullYear()} Painel Consulta
+        </footer>
       </body>
     </html>
   );
